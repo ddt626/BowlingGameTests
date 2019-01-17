4f2204c8ba5fb5bc2404f1e9fc9097116c900541
Raymond <ddt_df@hotmail.com>
2019-01-18 00:22:00 +0800

    pass test first round roll 2 and 6 get 8 point

--- BowlingGameTests/BowlingTest.cs
+++ BowlingGameTests/BowlingTest.cs
@@ -21,6 +21,14 @@ namespace BowlingGameTests
             ScoreShouldBe(2);
         }
 
+        [TestMethod]
+        public void FirstRound_Roll_2_6_Get_8()
+        {
+            _bowlingGame.Roll(2);
+            _bowlingGame.Roll(6);
+            ScoreShouldBe(8);
+        }
+
         private void ScoreShouldBe(int expected)
         {
             Assert.AreEqual(expected, _bowlingGame.Score());
